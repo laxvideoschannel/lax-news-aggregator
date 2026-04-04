a7c514553aa1fd9fad030e8e775ac130e3a368a4
laxvideoschannel <laxvideoschannel@gmail.com>
2026-04-03 20:07:18 -0400

    Delete app/next.config.js

--- app/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['rss-parser', '@supabase/supabase-js'],
-  },
-}
-
-module.exports = nextConfig
